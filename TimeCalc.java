0fa63ab4eb969b68132f9016e10d8d544b17fc2b
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-14 15:56:01 +0200

    2 try

--- TimeCalc.java
+++ TimeCalc.java
@@ -4,13 +4,13 @@ public class TimeCalc {
         int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
         int min = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int min_to_add = Integer.parseInt(args[1]);
-        System.out.println("hours: " + hours + " minutes: " + min + " minutes too add: " + min_to_add);
+        //System.out.println("hours: " + hours + " minutes: " + min + " minutes too add: " + min_to_add);
 
         //new hours and minutes to add
         int time = min_to_add + (hours * 60) + min;
         int new_hours = (time / 60) % 24;
         int new_min = (time - ((time/60)*60));
-        System.out.println(" new hour:" + new_hours + " new min: " + new_min); 
+        //System.out.println(" new hour:" + new_hours + " new min: " + new_min); 
         
         // print the new hour
         if ((new_hours < 10)) {
